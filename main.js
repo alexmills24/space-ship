8abd6a8828f9d4a6ac19d11a78a31edad448a65d
Alex Mills <alex.mills@Nology.local>
2019-12-04 12:36:42 +0000

    main function of the game is working. still needs some styling

---
+++ main.js
@@ -0,0 +1,81 @@
+class Ship {
+  constructor(name, life, damage) {
+    this.name = name;
+    this.life = life;
+    this.damage = damage;
+  };
+
+  reducePoints() {
+    this.life >= this.damage ? this.life -= this.damage : this.life = 0;
+  }
+  render() {
+    return `<div id="game-area"><p>${this.name}</p> <p>${this.life}</p> <p>${this.damage}</p></div>`
+  };
+};
+
+let ships = [];
+
+const makeMothership = () => {
+  for (index = 0; index < 1; index++) {
+    ships.push(new Ship("Mothership", 100, 9));
+  }
+}
+
+const makeDefence = () => {
+  for (index = 0; index < 5; index++) {
+    ships.push(new Ship("Defence-ship", 80, 10));
+  }
+};
+
+const makeAttack = () => {
+  for (index = 0; index < 8; index++) {
+    ships.push(new Ship("Attack-ship", 45, 12));
+  }
+};
+
+const shipsOnPage = () => {
+  let gameArea = document.getElementById("game-area");
+  gameArea.innerHTML = "";
+  ships.forEach(ship => {
+    gameArea.innerHTML += ship.render();
+  });
+};
+
+const startGame = () => {
+  location.reload();
+};
+const getRandomNumber = () => {
+  let random = Math.round(Math.random() * (ships.length - 1));
+  return random
+}
+
+const hitShip = () => {
+  let randomNumber = getRandomNumber();
+  if (ships[randomNumber].life !== 0) {
+    ships[randomNumber].reducePoints();
+  } else {
+    hitShip()
+  }
+  shipsOnPage();
+}
+
+const endGame = () => {
+  if (ships[0].life === 0) {
+    alert("game over")
+  } else {
+    hitShip()
+  }
+}
+
+document.getElementById("start-game").addEventListener("click", startGame);
+document.getElementById("hit").addEventListener("click", endGame);
+
+
+
+makeMothership();
+makeDefence();
+makeAttack();
+shipsOnPage();
+
+
+
---
+++ ships.js
@@ -0,0 +1,24 @@
+const ships = [
+    {
+      name: "Mothership",
+      life: 100,
+      damage: 9,
+      quantity: 1,
+    },
+
+    {
+        name: "Defence-Ship",
+        life: 80,
+        damage: 10,
+        quantity: 5,
+    },
+
+    {
+        name: "Attack-Ship",
+        life: 45,
+        damage: 12,
+        quantity: 8,
+    },
+];
+
+export default ships;
